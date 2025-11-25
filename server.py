015837b42e2d0f94ae6d905b5ccfa63db675486d
std67122420228-wq <std.67122420228@ubru.ac.th>
2025-11-25 11:21:54 +0700

    แก้ไข return function index เป็น สวัสดีชาวโลก

--- server.py
+++ server.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ =='__mine__':
-   app.run()
+@app.route('/')
+def index():
+   return "<h2>สวัสดีชาวโลก</h2>"
+
+if __name__ =='__main__':
+   app.run(debug=True)
